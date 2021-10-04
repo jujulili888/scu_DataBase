211db4425b588db5db2210288b1cfd5301b09381
jujulili888 <78644176+jujulili888@users.noreply.github.com>
2021-10-04 20:48:24 +0800

    Update q2_string_function.sql

--- homework2/q2_string_function.sql
+++ homework2/q2_string_function.sql
@@ -1,3 +1,3 @@
-SELECT DISTINCT shipName || "|" || substr(shipName,1,instr(shipName,'-')-1) 
+SELECT DISTINCT shipName || "|" || substr(shipName,1,instr(shipName,'-')-1) AS "Result Q2"
 FROM 'Order' 
 WHERE shipName LIKE "%-%" ORDER BY shipName;
